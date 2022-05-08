71b0e3db165e598ba448ee02239de9f915ce8ad4
Simon <simon.hirner99@gmail.com>
2022-05-08 13:23:20 +0200

    Compress text.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,29 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        filename: '[path][base].br',
+        algorithm: 'brotliCompress',
+        test: /\.js$/,
+      })
+    ]
+  },
+  devServer: {
+    before(app) {
+      app.use('*.js', (req, res, next) => {
+        if (req.get('Accept-Encoding')?.includes('br')) {
+          req.url += '.br'
+
+          res.set('Content-Encoding', 'br')
+          res.set('Content-Type', 'application/javascript; charset=utf-8')
+        }
+        next()
+      })
+    }
+  }
 }
